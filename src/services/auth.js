f8da0c373c983940af1732ad0e9785fb09fc3525
riot662006 <72505429+riot662006@users.noreply.github.com>
2023-11-05 00:44:51 -0500

    Basic Installations: Packages and FireBase

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCE3Yyr8KZVNgBFwfocOee-CuW_yhH5xk",
+  authDomain: "finnawalk-f8f43.firebaseapp.com",
+  projectId: "finnawalk-f8f43",
+  storageBucket: "finnawalk-f8f43.appspot.com",
+  messagingSenderId: "802765788953",
+  appId: "1:802765788953:web:8b2ebf72d22ec13fd25788"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
