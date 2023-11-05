1996aabdba719b11e18f84e71c17acf52f0c9ff0
okezieuc <okezieuc@users.noreply.github.com>
2023-11-05 01:47:07 -0600

    create route where users can schedule walks

---
+++ app/src/screens/scheduleWalk.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function ScheduleWalk() {
+  return (
+    <View>
+      <Text>Schedule a Walk</Text>
+    </View>
+  );
+}
+
+export default ScheduleWalk;
