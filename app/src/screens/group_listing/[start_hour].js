6b8782d5b88c234e9c525fcf2e9245ad013ad62a
okezieuc <okezieuc@users.noreply.github.com>
2023-11-05 04:40:29 -0600

    feat: load the reservations of group mates

--- app/src/screens/group_listing/[start_hour].js
+++ app/src/screens/group_listing/[start_hour].js
@@ -1,12 +1,54 @@
 import { useLocalSearchParams } from "expo-router";
+import { useState, useEffect } from "react";
 import { Text, View } from "react-native";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+
+import app from "../..//services/auth";
+const db = getFirestore(app);
 
 export default function Page() {
   const { start_hour } = useLocalSearchParams();
+  const [walkingGroupMembers, setWalkingGroupMembers] = useState([]);
+
+  // load all the reservations of people who are scheduled
+  // for start_hour
+
+  useEffect(() => {
+    async function loadReservationsForTheHour() {
+      const walkReservationsRef = collection(db, "session_reservations");
+      const reservationsForTheHourQuery = query(
+        walkReservationsRef,
+        where("day", "==", new Date().toDateString()),
+        where("startHour", "==", parseInt(start_hour))
+      );
+
+      const querySnapshot = await getDocs(reservationsForTheHourQuery);
+
+      const membersAccumulator = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        membersAccumulator.push(doc.data());
+      });
+
+      setWalkingGroupMembers(membersAccumulator);
+    }
+
+    loadReservationsForTheHour();
+  }, [start_hour]);
 
   return (
     <View>
       <Text>Walk Start Time: {start_hour}</Text>
+      {walkingGroupMembers.map((person) => (
+        <Text>{person.for}</Text>
+      ))}
     </View>
   );
 }
